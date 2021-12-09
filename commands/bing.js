749ec340af004a4775c50c2c404dcc21f7115685
Anthony Tran <ahutran@ucdavis.edu>
2021-12-09 03:39:26 -0800

    Added Bootstrap Code
    Added main.js that contains the bootstrap code as well as                                                                               a basic template of commands for the bot to recognize.

---
+++ commands/bing.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'bing',
+    description: "reply bong on message bing",
+    execute(message, args)
+    {
+        message.channel.send('bong');
+    }
+}
---
+++ main.js
@@ -0,0 +1,33 @@
+const { Client, Intents, DiscordAPIError } = require ('discord.js');
+const Discord = require ('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+const prefix = 'r!';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles)
+{
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+    console.log('Reminder Bot is ready');
+});
+
+client.on('messageCreate', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot)
+        return;
+    const args = message.content.slice(prefix.length).split("/ +/");
+    const command = args.shift().toLowerCase();
+
+    if(command === 'bing')
+        client.commands.get('bing').execute(message,args);
+});
+client.login('OTE4MzYyNzQ4NjY3MTc5MDIw.YbGJ9Q.PN59klxEGbJQfIS5AUvdLD3cFDU');
