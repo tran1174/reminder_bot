2fcc1306d60e1f09b0379c9a245a75523b27d114
Anthony Tran <ahutran@ucdavis.edu>
2021-12-12 05:53:07 -0800

    Updated Command Handler
    Reduction of if/else statements

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith(
 for(const file of commandFiles)
 {
     const command = require(`./commands/${file}`);
-
     client.commands.set(command.name, command);
 }
 
@@ -23,9 +22,9 @@ client.on('messageCreate', message => {
     if(!message.content.startsWith(prefix) || message.author.bot)
         return;
     const args = message.content.slice(prefix.length).split("/ +/");
-    const command = args.shift().toLowerCase();
+    const commandName = args.shift().toLowerCase();
+    const command = client.commands.get(commandName);
 
-    if(command === 'bing')
-        client.commands.get('bing').execute(message,args);
+    command.execute(message, args);
 });
 client.login(token);
